9df89229bd474fb16e36e4621e60dd68f193272a
TimSve14 <thesvensson@gmail.com>
2016-10-03 19:02:24 +0200

    Fixing controller

--- SensorsDemo/app/src/main/java/com/example/tim/sensorsdemo/MainActivity.java
+++ SensorsDemo/app/src/main/java/com/example/tim/sensorsdemo/MainActivity.java
@@ -23,10 +23,8 @@ import java.net.URISyntaxException;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
 
-    private float xAccel = 0.0f;
-    private float yAccel = 0.0f;
     private SensorManager sensorManager;
-    private String roomId = "1s6qouqjj5j8t8pbke29";
+    private String roomId = "byxkzgrlaoi6fwm6lxr";
     private String userName = "Android";
     private String currentState = "";
 
@@ -87,10 +85,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onSensorChanged(SensorEvent event) {
 
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            yAccel = -event.values[0];
-            xAccel = -event.values[1];
 
-            sendValue();
+
+                sendValue(event.values[0],event.values[1],event.values[2]);
+
         }
     }
 
@@ -116,126 +114,107 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
 
-    private void sendValue() {
+    private void sendValue(float xAccel, float yAccel, float zAccel){
 
         // To read the value of the sensor and send the right value to the server,
         // going to calibrate this later to fit the game
+        //System.out.println("X: " + xAccel);
+        //System.out.println("Y: " + yAccel);
+        //System.out.println("Z: " + zAccel);
+        //Standing still
+        if((xAccel > 3 && xAccel < 7) && (yAccel<2 && yAccel>-2)&& zAccel > 7&& currentState != "Center"){
+            System.out.println("Center");
+            mSocket.emit("send message",setJsonObject("C",roomId,userName));
+            currentState = "Center";
 
-        if(xAccel < -1.1 && xAccel > -3.9 && currentState != "Right0") {
+        }
+        //Right move
+        else if((xAccel<7 && xAccel>2) && (yAccel > 2&& yAccel < 5) && zAccel >2 && currentState != "Right0"){
             System.out.println("Right0");
-
-            JSONObject Right0 = setJsonObject("Right0",roomId,userName);
-
-           mSocket.emit("send message",Right0);
+            mSocket.emit("send message",setJsonObject("R0",roomId,userName));
             currentState = "Right0";
         }
 
-        if(xAccel < -4 && xAccel > -7 && currentState != "Right1"){
+        else if((xAccel<7 && xAccel>2) && (yAccel > 5&& yAccel < 9) && zAccel >2 && currentState != "Right1"){
             System.out.println("Right1");
-
-            JSONObject Right1 = setJsonObject("Right1",roomId,userName);
-            mSocket.emit("send message",Right1);
+            mSocket.emit("send message",setJsonObject("R1",roomId,userName));
             currentState = "Right1";
         }
-
-        if(xAccel < -7 && currentState != "Right2"){
-            System.out.println("Right2");
-
-            JSONObject Right2 = setJsonObject("Right2",roomId,userName);
-
-            mSocket.emit("send message",Right2);
-            currentState = "Right2";
+        // Down right
+        else if(((xAccel>7 && xAccel<9.5)  && (yAccel > 2&& yAccel < 5) && (zAccel <6 && zAccel >2 )) && currentState != "RightDown0"){
+            System.out.println("RightDown0");
+            mSocket.emit("send message",setJsonObject("RD0",roomId,userName));
+            currentState = "RightDown0";
         }
-
-        if(xAccel > -1 && xAccel < 1 && yAccel > -1 && yAccel < 1 && currentState != "Still"){
-            System.out.println("Standing still");
-
-            JSONObject Still = setJsonObject("Standing still",roomId,userName);
-            mSocket.emit("send message",Still);
-            currentState = "Still";
+        else if(((xAccel>7 && xAccel<9.5)  && (yAccel > 2&& yAccel < 5) && (zAccel <6 && zAccel <2 )) && currentState != "RightDown1"){
+            System.out.println("RightDown1");
+            mSocket.emit("send message",setJsonObject("RD1",roomId,userName));
+            currentState = "RightDown1";
         }
-
-        if(xAccel > 1.1 && xAccel < 3.9 && currentState != "Left0"){
-            System.out.println("Left0");
-
-            JSONObject Left0 = setJsonObject("Left0",roomId,userName);
-            mSocket.emit("send message",Left0);
-            currentState = "Left0";
-
+        else if(((xAccel>7 && xAccel<9.5)  && (yAccel > -5&& yAccel < -2) && (zAccel <6 && zAccel >2 )) && currentState != "LeftDown0"){
+            System.out.println("LeftDown0");
+            mSocket.emit("send message",setJsonObject("LD0",roomId,userName));
+            currentState = "LeftDown0";
         }
-
-        if(xAccel > 4 && xAccel < 7 && currentState != "Left1"){
-            System.out.println("Left1");
-
-            JSONObject Left1 = setJsonObject("Left1",roomId,userName);
-
-            mSocket.emit("send message",Left1);
-            currentState = "Left1";
+        else if(((xAccel>7 && xAccel<9.5)  && (yAccel > -9.5&& yAccel < -2) && (zAccel <6 && zAccel <2 )) && currentState != "LeftDown1"){
+            System.out.println("LeftDown1");
+            mSocket.emit("send message",setJsonObject("LD1",roomId,userName));
+            currentState = "LeftDown1";
         }
-
-        if(xAccel > 7 && currentState != "Left2"){
-            System.out.println("Left2");
-
-            JSONObject Left2 = setJsonObject("Left2",roomId,userName);
-
-            mSocket.emit("send message",Left2);
-            currentState = "Left2";
-        }
-
-        if(yAccel < -1.1 && yAccel > -3.9 && currentState != "Down0"){
+        // Down move
+        else if(((xAccel> 7 && xAccel <9) && (yAccel > -2 && yAccel < 2)&& zAccel >2) && currentState != "Down0"){
             System.out.println("Down0");
-
-            JSONObject Down0 = setJsonObject("Down0",roomId,userName);
-
-            mSocket.emit("send message",Down0);
+            mSocket.emit("send message",setJsonObject("D0",roomId,userName));
             currentState = "Down0";
-
         }
-
-        if(yAccel < -4 && yAccel > -7 && currentState != "Down1"){
+        else if((xAccel> 9 && (yAccel > -2 && yAccel < 2)&& zAccel >-2) && currentState != "Down1"){
             System.out.println("Down1");
-
-            JSONObject Down1 = setJsonObject("Down1",roomId,userName);
-
-            mSocket.emit("send message",Down1);
+            mSocket.emit("send message",setJsonObject("D1",roomId,userName));
             currentState = "Down1";
         }
-
-        if(yAccel < -7 && currentState != "Down2"){
-            System.out.println("Down2");
-
-            JSONObject Down2 = setJsonObject("Down2",roomId,userName);
-
-            mSocket.emit("send message",Down2);
-            currentState = "Down2";
+        // left move
+        else if((xAccel<7 && xAccel>2) && (yAccel < -2 && yAccel >-5)&& zAccel >2 && currentState != "Left0"){
+            System.out.println("Left0");
+            mSocket.emit("send message",setJsonObject("L0",roomId,userName));
+            currentState = "Left0";
         }
-
-        if(yAccel > 1.1 && yAccel < 3.9 && currentState != "Up0"){
+        else if((xAccel<7 && xAccel>3) && (yAccel < -5 && yAccel >-9)&& zAccel >2 && currentState != "Left1"){
+            System.out.println("Left1");
+            mSocket.emit("send message",setJsonObject("L1",roomId,userName));
+            currentState = "Left1";
+        }
+        else if(((xAccel < 2 && xAccel > -7 )&& (yAccel < -4 && yAccel > -9 )&& zAccel >7) && currentState != "LeftUp0"){
+            System.out.println("LeftUp0");
+            mSocket.emit("send message",setJsonObject("LU0",roomId,userName));
+            currentState = "LeftUp0";
+        }
+        else if(((xAccel < 2 && xAccel > -7 )&& (yAccel < -4 && yAccel > -9 )&& zAccel <7) && currentState != "LeftUp1"){
+            System.out.println("LeftUp1");
+            mSocket.emit("send message",setJsonObject("LU1",roomId,userName));
+            currentState = "LeftUp1";
+        }
+        else if(((xAccel < 2 && xAccel > -7 )&& (yAccel > 4 && yAccel < 9 )&& zAccel >7) && currentState != "RightUp0"){
+            System.out.println("RightUp0");
+            mSocket.emit("send message",setJsonObject("RU0",roomId,userName));
+            currentState = "RightUp0";
+        }
+        else if(((xAccel < 2 && xAccel > -7 )&& (yAccel > 4 && yAccel < 9 )&& zAccel <7) && currentState != "RightUp1"){
+            System.out.println("RightUp1");
+            mSocket.emit("send message",setJsonObject("RU1",roomId,userName));
+            currentState = "RightUp1";
+        }
+       // Up
+        else if(((xAccel< 1 && xAccel> -2 )&&(yAccel > -4 && yAccel < 4 )&& zAccel >2) && currentState != "Up0"){
             System.out.println("Up0");
-
-            JSONObject Up0 = setJsonObject("Up0",roomId,userName);
-
-            mSocket.emit("send message",Up0);
+            mSocket.emit("send message",setJsonObject("U0",roomId,userName));
             currentState = "Up0";
         }
-
-        if(yAccel > 4 && yAccel < 7 && currentState != "Up1"){
+        else if(((xAccel< -2 && xAccel> -7 )&&(yAccel > -4 && yAccel < 4 )&& zAccel >2) && currentState != "Up1"){
             System.out.println("Up1");
-
-            JSONObject Up1 = setJsonObject("Up1",roomId,userName);
-
-            mSocket.emit("send message",Up1);
+            mSocket.emit("send message",setJsonObject("U1",roomId,userName));
             currentState = "Up1";
         }
 
-        if(yAccel > 7 && currentState != "Up2"){
-            System.out.println("Up2");
-
-            JSONObject Up2 = setJsonObject("Up2",roomId,userName);
-
-            mSocket.emit("send message",Up2);
-            currentState = "Up2";
-        }
     }
 }
 
